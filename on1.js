0e1fefebd38fe60c2817796f153fd6992fb17dda
g-jaswanth15 <83649057+g-jaswanth15@users.noreply.github.com>
2021-08-25 02:01:53 +0530

    Add files via upload

--- on1.js
+++ on1.js
@@ -1,49 +1,64 @@
-
-
-var stockList = []
-
+var stockList = localStorage.getItem('list')?JSON.parse(localStorage.getItem('list')):[]
 
 document.getElementById('Add').addEventListener('click',()=>{
     var stockText = document.getElementById('stockText')
     stockList.push(stockText.value)
     console.log(stockList)
     adding()
-    // deleting()
 })
-var count = 0
+
 function adding(){
-    
 
     var li = document.createElement('li')
     li.className = 'text'
-    
+        
     var liText = document.createTextNode(stockText.value)
-    li.appendChild(liText)
+        li.appendChild(liText)
     
+    document.getElementById('stockList').appendChild(li)
+
+    localStorage.setItem('list',JSON.stringify(stockList))
+
+    var del = document.createElement('button')
+    del.className = 'delete'
+    del.appendChild(document.createTextNode('X'))
+    li.appendChild(del)
+
+    var delButtons = document.querySelectorAll('.delete')
+    delButtons.forEach((deleteStock) =>{
+        deleteStock.addEventListener('click',(event)=>{
+            var stockLi = event.target.parentElement
+            document.getElementById('stockList').removeChild(stockLi)
+            stockList.pop(stockText)
+            localStorage.setItem('list',JSON.stringify(stockList))
+        })
+    })
+}
+
+stored = JSON.parse(localStorage.getItem('list'))
+
+    stored.forEach(store=>{
+        var li = document.createElement('li')
+        li.className = 'text'
+        var liText = document.createTextNode(store)
+        li.appendChild(liText)
+     
     document.getElementById('stockList').appendChild(li)
 
     var del = document.createElement('button')
     del.className = 'delete'
     del.appendChild(document.createTextNode('X'))
     li.appendChild(del)
-    var delButtons = document.querySelectorAll('.delete')
-    console.log(delButtons)
-    delButtons.forEach((deleteStock) =>{
+    var delButtons1 = document.querySelectorAll('.delete1')
+    delButtons1.forEach((deleteStock) =>{
         deleteStock.addEventListener('click',(event)=>{
-            console.log('hi')
-            var stockLi = event.target.parentElement
-            console.log(stockLi)
-            document.getElementById('stockList').removeChild(stockLi)
+            var stockLi1 = event.target.parentElement
+            document.getElementById('stockList').removeChild(stockLi1)
             stockList.pop(stockText)
-            console.log(stockList)
-            count++;
-            console.log(count)
+            localStorage.setItem('list',JSON.stringify(stockList))
         })
     })
-}
-// function deleting(){
-    
-    
-// }
+})
+
 
 
